c15b0cea62aec50fcc41a841d5eca7addc51e18b
tarikaittoubdirte <t.aittoubdirte6049@uca.ac.ma>
2025-12-04 20:53:59 +0100

    Add files via upload

---
+++ search.js
@@ -0,0 +1,61 @@
+  let availableKeywords = [
+'Marrakech',
+'Agadir',
+'Ouarzazate',
+'Ifrane',
+'Rabat',
+'Fes',
+'Tanger',
+'Chefchaouen',
+];
+
+const cityPages = {
+    "Marrakech": "ville-marrakech.html",
+    "Agadir": "ville-agadir.html",
+    "Ouarzazate": "ville-Ouarzazate.html",
+    "Ifrane": "ville-Ifrane.html",
+    "Rabat": "ville-rabat.html",
+    "Fes": "ville-fes.html",
+    "Tanger": "ville-tanger.html",
+    "Chefchaouen": "ville-chefchaouen.html"
+};
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+if(input.length) {
+result = availableKeywords.filter((keyword)=>{
+           return keyword.toLowerCase().includes (input.toLowerCase());
+});
+console.log(result);
+}
+display(result);
+if(!result.length) {
+resultsBox.innerHTML = '';
+}
+}
+       function display(result) {
+    const content = result.map((city) => {
+        return `<li onclick="redirectToCity('${city}')">${city}</li>`;
+    });
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+function redirectToCity(city){
+    inputBox.value = city;
+    resultsBox.innerHTML = '';
+
+    // Redirection
+    if (cityPages[city]) {
+        window.location.href = cityPages[city];
+    } else {
+        alert("Page non trouvée pour : " + city);
+    }
+}
+
+function selectInput (list){
+inputBox.value = list.innerHTML;
+resultsBox.innerHTML = '';
+}
